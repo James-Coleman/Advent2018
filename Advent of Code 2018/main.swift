69463241ee14ee5a4d57d7beb442a50ac7782e1f
James Coleman <mrjameslcoleman@gmail.com>
2018-12-18 06:54:29 +0000

    Initial Commit

---
+++ Advent of Code 2018/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent of Code 2018
+//
+//  Created by James Coleman on 18/12/2018.
+//  Copyright © 2018 James Coleman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
