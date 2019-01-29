e2bdbb1a27d5f50b94feb8105f9854224ab57a5d
James Coleman <james.coleman@sonin.agency>
2019-01-29 16:58:23 +0000

    Refactored Day 9 into a simple function but getting the wrong result on the second example (first of the more examples)

--- Advent of Code 2018.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
+++ Advent of Code 2018.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
@@ -22,74 +22,84 @@ struct Player {
     */
 }
 
-
-
-var marbleBag = [Int](1...25)
-var marbleArray = [0]
-var currentMarbleIndex = 0
-/// Note that this is 1 indexed, not 0 indexed
-var currentPlayer = 1
-
-let seedArray = [Int](1...9)
-var players = seedArray.map { Player(id: $0/*, marbleCount: 25, playerCount: seedArray.count*/) }
-//players.forEach { print($0) }
-
-while let marble = marbleBag.first {
-    let thisMarble = marbleBag.remove(at: 0)
-    if thisMarble % 23 == 0 {
-        players[currentPlayer - 1].scoredMarbles += [thisMarble]
-        
-        let indexToRemove: Int = {
-           var newIndex = currentMarbleIndex
-            for i in 1...7 {
-                newIndex -= 1
-                if newIndex < 0 {
-                    newIndex = marbleArray.count - 1
-                }
-            }
-            return newIndex
-        }()
-        
-        let winningMarble = marbleArray.remove(at: indexToRemove + 1)
-        players[currentPlayer - 1].scoredMarbles += [winningMarble]
-        
-        let newMarbleIndex: Int = {
-            var newIndex = indexToRemove + 1
-            if newIndex >= marbleArray.count {
-                newIndex = 0
-            }
-            return newIndex
-        }()
-        
-        currentMarbleIndex = newMarbleIndex - 1
-    } else {
-        let newMarbleIndex: Int = {
-            var oldIndex = currentMarbleIndex
-            for i in 1...2 {
-                oldIndex += 1
-                if oldIndex >= marbleArray.count {
-                    oldIndex = 0
-                }
-            }
-            return oldIndex
-        }()
-        
-        marbleArray.insert(thisMarble, at: newMarbleIndex + 1)
-        currentMarbleIndex = newMarbleIndex
-    }
-    
-    currentPlayer += 1
-    if currentPlayer > players.count {
-        currentPlayer = 1
-    }
-    
-    print(marbleArray) // This confirms that each stage matches the example
+enum PlayerError: Swift.Error {
+    case noPlayers
 }
 
-let playersByScore = players.sorted { $0.score > $1.score }
-if let highestScoringPlayer = playersByScore.first {
+func highestScore(playerCount: Int, marbleCount: Int) throws -> Int {
+    var marbleBag = [Int](0...marbleCount)
+    var marbleArray = [marbleBag.remove(at: 0)]
+    var currentMarbleIndex = 0
+    /// Note that this is 1 indexed, not 0 indexed
+    var currentPlayer = 1
+    
+    let seedArray = [Int](1...9)
+    var players = seedArray.map { Player(id: $0) }
+    
+    while let marble = marbleBag.first {
+        let thisMarble = marbleBag.remove(at: 0)
+        if thisMarble % 23 == 0 {
+            players[currentPlayer - 1].scoredMarbles += [thisMarble]
+            
+            let indexToRemove: Int = {
+                var newIndex = currentMarbleIndex
+                for i in 1...7 {
+                    newIndex -= 1
+                    if newIndex < 0 {
+                        newIndex = marbleArray.count - 1
+                    }
+                }
+                return newIndex
+            }()
+            
+            let winningMarble = marbleArray.remove(at: indexToRemove + 1)
+            players[currentPlayer - 1].scoredMarbles += [winningMarble]
+            
+            let newMarbleIndex: Int = {
+                var newIndex = indexToRemove + 1
+                if newIndex >= marbleArray.count {
+                    newIndex = 0
+                }
+                return newIndex
+            }()
+            
+            currentMarbleIndex = newMarbleIndex - 1
+        } else {
+            let newMarbleIndex: Int = {
+                var oldIndex = currentMarbleIndex
+                for i in 1...2 {
+                    oldIndex += 1
+                    if oldIndex >= marbleArray.count {
+                        oldIndex = 0
+                    }
+                }
+                return oldIndex
+            }()
+            
+            marbleArray.insert(thisMarble, at: newMarbleIndex + 1)
+            currentMarbleIndex = newMarbleIndex
+        }
+        
+        currentPlayer += 1
+        if currentPlayer > players.count {
+            currentPlayer = 1
+        }
+        
+//        print(marbleArray) // This confirms that each stage matches the example
+    }
+    
+    let playersByScore = players.sorted { $0.score > $1.score }
+    
+    guard let highestScoringPlayer = playersByScore.first else { throw PlayerError.noPlayers }
+    
     print(highestScoringPlayer)
     print(highestScoringPlayer.score)
+    
+    return highestScoringPlayer.score
+    
 }
+
+let example0 = highestScore(playerCount: 9, marbleCount: 25)
+let example1 = highestScore(playerCount: 10, marbleCount: 1618) // Currently returning 10132 but should be 8317
 
 
